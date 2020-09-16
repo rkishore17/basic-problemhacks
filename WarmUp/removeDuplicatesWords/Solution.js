f5bbd7763ce6fd8f48a4ad6a81a14c860edda128
Ravi Kishore S <ravi.s@tavant.com>
2020-09-16 21:48:53 +0530

    removing duplicates from word

---
+++ WarmUp/removeDuplicatesWords/Solution.js
@@ -0,0 +1,27 @@
+//Problem: Removing the duplicates by words or characters.
+
+/*
+Example:
+Input: "A long lived wolf, lived in himalayas"
+Output: "A long lived wolf, in himalayas"
+*/
+
+//Here, we can remove by characters by removing the split method.
+
+function removeDuplicates(sentence) {
+  let strArr = sentence.split(" ");
+
+  let map = new Map();
+
+  for (let i = 0; i < strArr.length; i++) {
+    if (!map.has(strArr[i])) {
+      map.set(strArr[i], 1);
+    }
+  }
+
+  for (let [k] of map) console.log(k);
+}
+let strValue = "A long lived wolf, lived in himalayas";
+// let strValue = "codeForBetterWorld".toLowerCase();
+removeDuplicates(strValue);
+
